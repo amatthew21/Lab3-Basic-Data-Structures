88f41946699d961047c4fe1fdf9c044078382b5c
ctjac <ctjac@Aqua.fios-router.home>
2023-02-06 14:20:03 -0500

    Renamed to Stacks

---
+++ Lab3/src/stack/Stacks.java
@@ -0,0 +1,12 @@
+package stack;
+
+import java.util.Stack;
+
+public class Stacks {
+	//Task 6: Stacks By Aqua Jacinto
+	public static void main(String[] args) {
+		Stack<String> stringStack = new Stack<>();
+
+	}
+
+}
